2c15e82433ba82a55575586fdb9bf89af5ea8382
HaiTran <haitran@HaiTrans-MacBook-Pro.local>
2026-03-17 14:32:14 +0700

    fix: correct keyboardVerticalOffset to positive value for Android

--- src/sform/answers/AnswerAddress.tsx
+++ src/sform/answers/AnswerAddress.tsx
@@ -42,7 +42,7 @@ function SearchModal({
       <KeyboardAvoidingView
         behavior="padding"
         style={styles.overlay}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 70 : -300}>
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 70 : 150}>
         <View style={styles.sheet}>
           <Text style={styles.sheetTitle}>{title}</Text>
           <TextInput
